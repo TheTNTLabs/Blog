0372d2abe75b33e6f196facd2ea72a963fe72027
Chewy <Chewy@noreply.codeberg.org>
2023-12-23 20:20:49 -0500

    clean urls

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,9 +11,21 @@ DEFAULT_LANG = 'en-us'
 TIMEZONE = 'Etc/UTC'
 
 MENUITEMS = (('Home', 'https://www.thetntlabs.us.to/', '_blank'),
-             ('Archives', '/archives.html', '_self'),
+             ('Archives', '/archives/', '_self'),
              ('Feed', '/feeds/all.atom.xml', '_self'))
 
 DEFAULT_PAGINATION = 10
 
 DELETE_OUTPUT_DIRECTORY = True
+
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
+ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+
+AUTHOR_SAVE_AS = 'author/{slug}.html'
+AUTHOR_URL = 'author/{slug}/'
+
+CATEGORY_SAVE_AS = 'category/{slug}.html'
+CATEGORY_URL = 'category/{slug}/'
+
+TAG_SAVE_AS = 'tag/{slug}.html'
+TAG_URL = 'tag/{slug}/'
