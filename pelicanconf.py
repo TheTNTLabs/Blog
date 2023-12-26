1455a383c093f544538446bc6c34c60ce63f98ab
Chewy <Chewy@noreply.codeberg.org>
2023-12-26 18:22:27 -0500

    remove trailing slash

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,7 +11,7 @@ DEFAULT_LANG = 'en-us'
 TIMEZONE = 'Etc/UTC'
 
 MENUITEMS = (('Home', 'https://www.thetntlabs.us.to/', '_blank'),
-             ('Archives', '/archives/', '_self'),
+             ('Archives', '/archives', '_self'),
              ('Feed', '/feeds/all.atom.xml', '_self'))
 
 DEFAULT_PAGINATION = 10
@@ -19,13 +19,13 @@ DEFAULT_PAGINATION = 10
 DELETE_OUTPUT_DIRECTORY = True
 
 ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
-ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}'
 
 AUTHOR_SAVE_AS = 'author/{slug}.html'
-AUTHOR_URL = 'author/{slug}/'
+AUTHOR_URL = 'author/{slug}'
 
 CATEGORY_SAVE_AS = 'category/{slug}.html'
-CATEGORY_URL = 'category/{slug}/'
+CATEGORY_URL = 'category/{slug}'
 
 TAG_SAVE_AS = 'tag/{slug}.html'
-TAG_URL = 'tag/{slug}/'
+TAG_URL = 'tag/{slug}'
