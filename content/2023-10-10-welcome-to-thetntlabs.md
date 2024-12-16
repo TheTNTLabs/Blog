340ff47a5c0166d686839960b2bf781de32ac331
AgileLlama <agilellama@noreply.codeberg.org>
2024-12-16 00:01:07 -0500

    modify design

--- content/2023-10-10-welcome-to-thetntlabs.md
+++ content/2023-10-10-welcome-to-thetntlabs.md
@@ -1,6 +1,9 @@
 Title: Welcome to TheTNTLabs!
 Date: 2023-10-10 00:00
-Category: Announcements
 Authors: TheTNTLabs
+Category: Announcements
+Image: https://design.thetntlabs.us.to/img/hero-1920x650.png
+Image_Alt: TheTNTLabs Gate
+Discuss: https://thetntlabs.zulipchat.com/#narrow/channel/455122-announcements/topic/Welcome.20to.20TheTNTLabs!
 
 ...
--- pelicanconf.py
+++ pelicanconf.py
@@ -1,11 +1,19 @@
+from pelican import __version__
+PELICAN_VERSION = __version__
+
 AUTHOR = 'TheTNTLabs Gaming'
 SITENAME = 'TheTNTLabs Blog'
 SITEURL = 'https://blog.thetntlabs.us.to'
+PARENTNAME = 'TheTNTLabs'
+PARENTURL = 'https://www.thetntlabs.us.to/'
+MENUITEMS = (('Blog', '/', '_self', True),
+             ('Chat', 'https://thetntlabs.zulipchat.com/', '_blank', False),
+             ('Source', 'https://codeberg.org/TheTNTLabs', '_blank', False),
+             ('Status', 'https://status.thetntlabs.us.to/', '_self', False))
 
 PATH = 'content/'
 OUTPUT_PATH = 'static-deployment/'
 THEME = 'themes/thetntlabs/'
-
 STATIC_PATHS = [
     'static/_headers',
     ]
@@ -13,25 +21,24 @@ EXTRA_PATH_METADATA = {
     'static/_headers': {'path': '_headers'},
     }
 
+DEFAULT_PAGINATION = 9
 DEFAULT_LANG = 'en-us'
 DEFAULT_DATE_FORMAT = '%B %d, %Y'
-TIMEZONE = 'Etc/UTC'
-
-MENUITEMS = (('Home', 'https://www.thetntlabs.us.to/', '_blank'),
-             ('Archives', '/archives', '_self'),
-             ('Authors', '/authors', '_self'),
-             ('Tags', '/tags', '_self'),
-             ('Feed', '/feeds/all.atom.xml', '_self'))
-
-DEFAULT_PAGINATION = 10
+TIMEZONE = 'America/New_York'
 
 DELETE_OUTPUT_DIRECTORY = True
 
-ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
-ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}'
+ARTICLE_SAVE_AS = 'article/{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
+ARTICLE_URL = 'article/{date:%Y}/{date:%m}/{date:%d}/{slug}'
 
-ARTICLE_LANG_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}-{lang}.html'
-ARTICLE_LANG_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}-{lang}'
+ARTICLE_LANG_SAVE_AS = 'article/{date:%Y}/{date:%m}/{date:%d}/{slug}-{lang}.html'
+ARTICLE_LANG_URL = 'article/{date:%Y}/{date:%m}/{date:%d}/{slug}-{lang}'
+
+YEAR_ARCHIVE_SAVE_AS = 'archive/{date:%Y}/index.html'
+YEAR_ARCHIVE_URL = 'archive/{date:%Y}/'
+
+MONTH_ARCHIVE_SAVE_AS = 'archive/{date:%Y}/{date:%m}/index.html'
+MONTH_ARCHIVE_URL = 'archive/{date:%Y}/{date:%m}/'
 
 AUTHOR_SAVE_AS = 'author/{slug}.html'
 AUTHOR_URL = 'author/{slug}'
@@ -41,3 +48,8 @@ CATEGORY_URL = 'category/{slug}'
 
 TAG_SAVE_AS = 'tag/{slug}.html'
 TAG_URL = 'tag/{slug}'
+
+PAGINATION_PATTERNS = (
+    (1, '{base_name}', '{save_as}'),
+    (2, '{base_name}/page/{number}/', '{base_name}/page/{number}/index.html'),
+)
